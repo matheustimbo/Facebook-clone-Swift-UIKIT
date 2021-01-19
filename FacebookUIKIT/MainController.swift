a68f95048cbc530c6b3d15eb9047f4c5cd73da18
Matheus Timbó <matheusfild4@hotmail.com>
2021-01-19 12:08:53 -0300

    Adding project

--- FacebookUIKIT/MainController.swift
+++ FacebookUIKIT/MainController.swift
@@ -5,4 +5,168 @@
 //  Created by Matheus Timbó on 18/01/21.
 //
 
-import Foundation
+import UIKit
+import LBTATools
+
+class PostCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(image: UIImage(named: "1"));
+    
+    let nameLabel = UILabel(text: "Name Label")
+    let dateLabel = UILabel(text: "Friday at 11:11AM")
+    let postTextLabel = UILabel(text: "Here is my post text")
+    //let imageViewGrid = UIView(backgroundColor: .yellow)
+    
+    let photosGridController = PhotosGridController()
+    
+    override func setupViews() {
+        backgroundColor = .white
+        stack(
+            hstack(
+                imageView.withHeight(40).withWidth(40),
+                stack(nameLabel, dateLabel),
+                spacing: 8
+            )
+        .padLeft(12).padRight(12).padTop(12),postTextLabel,photosGridController.view, spacing: 8)
+    }
+}
+
+class StoryHeader: UICollectionReusableView {
+    
+    let storiesController = StoriesController(scrollDirection: .horizontal)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        stack(storiesController.view)
+    }
+    
+    required init?(coder:NSCoder) {
+        fatalError()
+    }
+}
+
+class StoryPhotoCell: LBTAListCell<String> {
+    
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+        }
+    }
+    
+    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "Davi Pinho", font: .boldSystemFont(ofSize: 14), textColor: .white)
+    
+    override func setupViews() {
+        imageView.layer.cornerRadius = 10
+        stack(imageView)
+        
+        setupGradientLayer()
+        
+        stack(UIView(),nameLabel).withMargins(.allSides(8))
+    }
+    
+    let gradientLayer = CAGradientLayer()
+    
+    fileprivate func setupGradientLayer() {
+        
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.7, 1.2]
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+}
+
+class StoriesController: LBTAListController<StoryPhotoCell, String>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 100, height: view.frame.height - 24)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 12, bottom: 0, right: 12)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.items = ["1", "2", "3", "4", "5"]
+    }
+}
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
+        
+        self.items = ["Hello", "World","Hello", "World","Hello", "World","Hello", "World"]
+        
+        setupNavBar()
+    }
+    
+    let fbLogoImageView = UIImageView(image: UIImage(named: "fb_logo"), contentMode: .scaleAspectFit)
+    
+    let searchButton = UIButton(title: "Search", titleColor: .red)
+    
+    fileprivate func setupNavBar() {
+        let width = UIScreen.main.bounds.width
+        
+        let titleView = UIView(backgroundColor: .clear)
+        titleView.frame = .init(x: 0, y: 0, width: width, height: 50 )
+        
+        
+        titleView.hstack(fbLogoImageView.withWidth(120), UIView(backgroundColor: .clear).withWidth(width),searchButton.withWidth(60))
+        
+        navigationItem.titleView = titleView
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let magicalSafeAreaTop: CGFloat = 88
+        
+        let offset = scrollView.contentOffset.y + 88
+        
+        let alpha: CGFloat = 1 - ((scrollView.contentOffset.y + magicalSafeAreaTop)/magicalSafeAreaTop)
+        
+        searchButton.alpha = alpha
+        fbLogoImageView.alpha = alpha
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 430)
+    }
+}
+
+import SwiftUI
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return UINavigationController(rootViewController: MainController())
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+}
--- FacebookUIKIT/PhotosGridController.swift
+++ FacebookUIKIT/PhotosGridController.swift
@@ -5,4 +5,74 @@
 //  Created by Matheus Timbó on 18/01/21.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
+import LBTATools
+
+class PhotoGridCell: LBTAListCell<String> {
+    
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+        }
+    }
+    
+    let imageView = UIImageView(image: UIImage(named: "1"), contentMode: .scaleAspectFill)
+    
+    override func setupViews() {
+        backgroundColor = .yellow
+        stack(imageView)
+    }
+}
+
+class PhotosGridController: LBTAListController<PhotoGridCell, String>, UICollectionViewDelegateFlowLayout {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .lightGray
+        self.items = ["1","2","3","4", "5"]
+    }
+    
+    let cellSpacing: CGFloat = 4
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        if (indexPath.item == 0 || indexPath.item == 1){
+            let width =  (view.frame.width - 3 * cellSpacing) / 2
+            return .init(width: width, height: width)
+        }
+        
+        let width =  (view.frame.width - 4.1 * cellSpacing) / 3
+        return .init(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: cellSpacing, bottom: 0, right: cellSpacing)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing;
+    }
+}
+
+class PhotosGridPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return PhotosGridController()
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+    
+}
+
